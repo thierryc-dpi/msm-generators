2dcde0d9c080372bf402b671ba123896124949a0
Thierry Chau <thierry.chau@gmail.com>
2023-10-09 16:40:16 +0000

    generate movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_09_163713) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_09_163948) do
   create_table "actors", force: :cascade do |t|
     t.string "name"
     t.date "dob"
@@ -24,6 +24,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_09_163713) do
     t.string "name"
     t.date "dob"
     t.text "bio"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "year"
+    t.integer "duration"
+    t.integer "director_id"
+    t.text "description"
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
